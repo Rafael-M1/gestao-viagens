5dbcad66a958374346a9019ff3413154ec8061cb
rafael-m1 <rafael.muzzi2@gmail.com>
2025-07-29 13:46:12 -0400

    Changes initial route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\VehicleModelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
@@ -12,9 +15,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //-- Breeze Routes --
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //-- Breeze Routes --
+
+    Route::resource('vehicles', VehicleController::class)->except(['show']);
+    Route::get('/vehicles/pdf', [VehicleController::class, 'exportPdf'])->name('vehicles.pdf');
+    Route::resource('brands', BrandController::class)->except(['show']);
+    Route::resource('vehicle-models', VehicleModelController::class)->except(['show']);
 });
 
 require __DIR__.'/auth.php';
