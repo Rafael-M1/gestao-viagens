2c6a7cab368f9451706bfcf6e1a2dd6331c151b3
rafael-m1 <rafael.muzzi2@gmail.com>
2025-07-29 13:32:51 -0400

    Adds Roles Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
             BrandSeeder::class,
             VehicleModelSeeder::class,
             VehicleSeeder::class,
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::firstOrCreate(['name' => 'Admin']);
+        $userRole = Role::firstOrCreate(['name' => 'User']);
+
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('12345678'),
+            ]
+        );
+
+        $user = User::firstOrCreate(
+            ['email' => 'user@example.com'],
+            [
+                'name' => 'Usuário Padrão',
+                'password' => Hash::make('12345678'),
+            ]
+        );
+
+        $admin->roles()->syncWithoutDetaching([$adminRole->id]);
+        $user->roles()->syncWithoutDetaching([$userRole->id]);
+    }
+}
