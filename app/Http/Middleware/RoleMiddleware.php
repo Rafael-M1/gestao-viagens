99fd1ea33286406a706ca7e871184ea3055942bf
rafael-m1 <rafael.muzzi2@gmail.com>
2025-07-31 15:18:17 -0400

    Adds types to verifyAllRoles function

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
@@ -23,7 +24,8 @@ class RoleMiddleware
         return $next($request);
     }
 
-    private function verifyAllRoles($userRoles, ...$requiredRoles) {
+    private function verifyAllRoles(Collection $userRoles, array $requiredRoles): bool
+    {
         $requiredRoles = Arr::flatten($requiredRoles);
         if ($userRoles instanceof \Illuminate\Support\Collection) {
             $userRoles = $userRoles->pluck('name')->toArray();
